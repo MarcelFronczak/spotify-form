d1b4d8594f69b771cb6612ef7a80c6b2c7a850ad
Marcel Fronczak <marcel.fronczak@gmail.com>
2023-02-26 17:38:54 +0100

    styling

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./global-styles/style.scss";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+   </React.StrictMode>
+  </BrowserRouter>
+
 );
