28ffac1164380b898d3a699aabdacfcfcb2e699d
Marcel Fronczak <marcel.fronczak@gmail.com>
2023-02-26 17:52:06 +0100

    google button added

--- src/components/registration/Registration.tsx
+++ src/components/registration/Registration.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFacebook } from '@fortawesome/free-brands-svg-icons'
+import { faFacebook, faGoogle } from '@fortawesome/free-brands-svg-icons'
 
 const Registration: React.FC = () => {
   return (
@@ -9,6 +9,12 @@ const Registration: React.FC = () => {
         <div>
           <FontAwesomeIcon icon={faFacebook} className='facebook_icon'/>
           <p className='font_l'>Sign up with Facebook</p>
+        </div>
+      </a>
+      <a href="#" className="button_google">
+        <div>
+          <FontAwesomeIcon icon={faGoogle} className='google_icon'/>
+          <p className='font_l'>Sign up with Google</p>
         </div>
       </a>
     </div>
