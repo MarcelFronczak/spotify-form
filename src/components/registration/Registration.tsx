46506d313c3a8c9a64c27f674ca939cf721073da
Marcel Fronczak <marcel.fronczak@gmail.com>
2023-02-26 17:39:13 +0100

    facebook button

--- src/components/registration/Registration.tsx
+++ src/components/registration/Registration.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebook } from '@fortawesome/free-brands-svg-icons'
 
 const Registration: React.FC = () => {
   return (
     <div>
-      
+      <a href="#" className="button_facebook">
+        <div>
+          <FontAwesomeIcon icon={faFacebook} className='facebook_icon'/>
+          <p className='font_l'>Sign up with Facebook</p>
+        </div>
+      </a>
     </div>
   )
 }
